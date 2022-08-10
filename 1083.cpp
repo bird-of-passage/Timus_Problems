860bebf7185002f5631dcf3619a9b15efff7c5cb
Invincible-21 <shantobd1969@gmail.com>
2022-08-10 16:00:55 +0600

    updated

--- 1083.cpp
+++ 1083.cpp
@@ -1,30 +1,32 @@
 #include <iostream>
+
 using namespace std;
 
 int main()
 {
-    char ch;
-    int n, k = 0, f = 1;
-    cin >> n >> ch;
+    int n, k = 0;
+    cin >> n;
 
-    if (ch == '!')
+    char c;
+
+    while (true)
     {
-        cout << ch;
-        k++;
-    }
-    else if (ch == '\n')
-    {
-        cout << ch;
-    }
-    for (n = 1; n <= 10; n++)
-    {
-        f = f * (n - 2 * k);
-        if (f <= 0)
-        {
+        c = getc(stdin);
+        if (c == '\n')
             break;
-        }
-        cout << f << endl;
+        if (c == '!')
+            ++k;
     }
+
+    int fact = 1;
+
+    while (n > 0)
+    {
+        fact *= n;
+        n -= k;
+    }
+
+    cout << fact << endl;
 
     return 0;
 }
