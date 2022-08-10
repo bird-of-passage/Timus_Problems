f9cc966b2b97b8c9852edd5471ae2e5aa2a6b727
Invincible-21 <shantobd1969@gmail.com>
2022-08-10 15:36:40 +0600

    tough

--- 1083.cpp
+++ 1083.cpp
@@ -3,6 +3,28 @@ using namespace std;
 
 int main()
 {
+    char ch;
+    int n, k = 0, f = 1;
+    cin >> n >> ch;
+
+    if (ch == '!')
+    {
+        cout << ch;
+        k++;
+    }
+    else if (ch == '\n')
+    {
+        cout << ch;
+    }
+    for (n = 1; n <= 10; n++)
+    {
+        f = f * (n - 2 * k);
+        if (f <= 0)
+        {
+            break;
+        }
+        cout << f << endl;
+    }
 
     return 0;
 }
